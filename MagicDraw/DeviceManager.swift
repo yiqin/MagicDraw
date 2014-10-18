c3911399be78b485225876668cfbbf8a0da7c33c
yiqin <yiqin.mems@gmail.com>
2014-10-18 13:51:35 -0400

    Alamofire and Text Recognition work now.

---
+++ MagicDraw/DeviceManager.swift
@@ -0,0 +1,19 @@
+//
+//  DeviceManager.swift
+//  MagicDraw
+//
+//  Created by yiqin on 10/18/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+import Foundation
+
+struct ScreenSize {
+    static var width = 320
+    static var height = 960
+}
+
+
+class DeviceManager {
+    
+}
--- MagicDraw/ViewController.swift
+++ MagicDraw/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set
+        ScreenSize.width = 500
+        ScreenSize.height = 800
+        
+        // Get
+        let width = ScreenSize.width
+        println("My view state:\(width)")
+        
+        
+        let URL=NSURL(string: "https://imagevision-text-search-v1.p.mashape.com/textSearch/detectText")
+        
+        Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders = ["X-Mashape-Key": "xiL7zSTisvmshlzqU2b8HimW98NFp1MvblHjsnGVIXnFab2CzB",
+        "Content-Type": "application/x-www-form-urlencoded"]
+        
+        var params = [
+            "objecturl": "http://files.parsetfss.com/acbc822b-b4c4-4b64-9e34-aa2001e5b251/tfss-422ec0e5-21cd-430f-9e4b-aa02142e4efa-IMG_1097.JPG"
+        ]
+        
+        let manager = Alamofire.Manager.sharedInstance // or create a new one
+        let request = Alamofire.request(.POST, URL, parameters: params, encoding: .JSON)
+        request.responseString { (request, response, string, error) in
+            // ...
+            
+            if let result = string {
+                println(result)
+                // process result
+            } else if let theError = error {
+                println(theError.localizedDescription)
+            }
+        }
+        
+        
         
     }
 
